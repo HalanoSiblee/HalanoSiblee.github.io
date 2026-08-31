2248261e7db728408b5de493f4a97370d7a3f527
halanosiblee <HalanoSiblee@gmail.com>
2026-08-31 16:37:22 +0300

    projectAdjust

---
+++ projects.js
@@ -0,0 +1,10 @@
+// proj.js
+const projectLinks = `
+https://github.com/HalanoSiblee/Fl-archpkg-browser
+https://github.com/HalanoSiblee/XlaserX
+https://github.com/HalanoSiblee/RaymothFM
+https://github.com/HalanoSiblee/WebThinker
+https://github.com/HalanoSiblee/tuicharmap
+https://github.com/HalanoSiblee/linux95apps
+https://github.com/HalanoSiblee/nmusage
+`;
