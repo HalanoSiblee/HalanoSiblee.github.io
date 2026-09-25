0f86355932a7b1cbf94752652eabf1430c253eaf
halanosiblee <HalanoSiblee@gmail.com>
2026-09-25 10:56:21 +0300

    proj

--- projects.js
+++ projects.js
@@ -1,5 +1,6 @@
 // proj.js
 const projectLinks = `
+https://github.com/HalanoSiblee/mothcc
 https://github.com/HalanoSiblee/MothMath-expression
 https://github.com/HalanoSiblee/XCropper
 https://github.com/HalanoSiblee/Mothplayer
